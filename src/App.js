7d909eb1d2aa494a9f20d31ce01a90565bde72bb
Douglas Obara <dobara@kabarak.ac.ke>
2021-06-26 21:01:26 +0300

    contact component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./component/Home";
 import About from "./component/About";
 import Skills from "./component/Skills";
 import Works from "./component/Works";
+import Contact from "./component/Contact";
 import Footer from "./component/Footer";
 import "./App.css";
 import NavBar from "./component/NavBar";
@@ -17,6 +18,7 @@ function App() {
           <Route exact path="/about" component={About} />
           <Route exact path="/skills" component={Skills} />
           <Route exact path="/works" component={Works} />
+          <Route exact path="/contact" component={Contact} />
         </Switch>
         <Footer />
       </div>
---
+++ src/component/Contact/index.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./style.css";
+
+const Contact = () => {
+  return (
+    <div className="contact">
+      <h1>Contact me!</h1>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.7761302987788!2d36.77815591487524!3d-1.3096160360217441!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f1a794d2ec013%3A0x44f5d235fd4bf605!2sKibera%20Dr%2C%20Nairobi!5e0!3m2!1sen!2ske!4v1624729119537!5m2!1sen!2ske"
+        frameBorder="0"
+        allowFullScreen=""
+        aria-hidden="false"
+        tabIndex="0"
+      ></iframe>
+
+      <h4>Phone</h4>
+      <p>+2547 42 608 717</p>
+      <h4>Email</h4>
+      <p>douglasobara97@gmail.com</p>
+      <h4>Address</h4>
+      <p>34567, NAIROBI 00100.</p>
+    </div>
+  );
+};
+
+export default Contact;
